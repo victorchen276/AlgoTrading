8555adbff2cbd8a439728c7fa9d3b7811eef3803
Victor Chen <victor.chen@spark.co.nz>
2022-04-03 23:45:06 +1200

    add contract

--- IBDemoProject/main.py
+++ IBDemoProject/main.py
@@ -1,6 +1,7 @@
 
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
 
 class TradingApp(EWrapper, EClient):
     def __init__(self):
@@ -9,11 +10,21 @@ class TradingApp(EWrapper, EClient):
     def error(self, reqId, errorCode, errorString):
         print("Error {} {} {}".format(reqId, errorCode, errorString))
 
+    def contractDetails(self, reqId, contractDetails):
+        print("reqID: {}, contract:{}".format(reqId, contractDetails))
 
 app = TradingApp()
 app.connect("127.0.0.1", 7497, clientId=0)
-app.run()
 
+contract = Contract()
+contract.symbol = "AAPL"
+contract.secType = "STK"
+contract.currency = "USD"
+contract.exchange ="SMART"
+app.reqContractDetails(100, contract)
+
+
+app.run()
 
 
 
