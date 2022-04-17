4baf9bebd1cfc0638b2b3115d838395d2d52e8a4
Victor Chen <victorchen276@gmail.com>
2022-04-17 21:54:57 +1200

    historical data

--- IBDemoProject/main.py
+++ IBDemoProject/main.py
@@ -1,7 +1,10 @@
 
 from ibapi.client import EClient
+from ibapi.common import BarData
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
+
+from datetime import datetime
 
 class TradingApp(EWrapper, EClient):
     def __init__(self):
@@ -13,6 +16,9 @@ class TradingApp(EWrapper, EClient):
     def contractDetails(self, reqId, contractDetails):
         print("reqID: {}, contract:{}".format(reqId, contractDetails))
 
+    def historicalData(self, reqId: int, bar: BarData):
+        print("HostroicalData. RegId:", reqId, "BarData.", bar)
+
 app = TradingApp()
 app.connect("127.0.0.1", 7497, clientId=0)
 
@@ -20,13 +26,26 @@ contract = Contract()
 contract.symbol = "AAPL"
 contract.secType = "STK"
 contract.currency = "USD"
-contract.exchange ="SMART"
-app.reqContractDetails(100, contract)
+contract.exchange ="ISLAND"
+# app.reqContractDetails(100, contract)
 
+# app.reqHistoricalData(reqId=1,
+#                       contract=contract,
+#                       endDateTime='',
+#                       durationStr='3 M',
+#                       barSizeSetting='5 mins',
+#                       whatToShow='MIDPOINT',
+#                       useRTH=1,
+#                       formatDate=1,
+#                       keepUpToDate=0,
+#                       chartOptions=[])
+
+queryTime = (datetime.datetime.today() - datetime.timedelta(days=180)).strftime("%Y%m%d %H:%M:%S")
+app.reqHistoricalData(4103, contract, queryTime, "1 M", "1 day", "SCHEDULE", 1, 1, False, [])
 
 app.run()
 
-
+print('aaaa')
 
 
 
