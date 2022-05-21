2fef177dc5f21f4a940861d1c2b0d4c9ba7d1946
Victor Chen <victorchen276@gmail.com>
2022-05-21 16:10:27 +1200

    update

--- PolygonDemo/main.py
+++ PolygonDemo/main.py
@@ -6,17 +6,22 @@ from dotenv import load_dotenv
 import os
 
 
+
 def configure():
     load_dotenv()
 
 def main():
     configure()
-    print("aaaaa"+os.getenv('api_key'))
-
-# client = RESTClient()
-#
-# financials = client.get_ticker_details("NFLX")
-# print(financials)
+    # print(os.getenv('POLYGON_API_KEY'))
+    client = RESTClient(api_key=os.getenv('POLYGON_API_KEY'))
+    # financials = client.get_ticker_details('NFLX')
+    # print(financials)
+    aggs = client.get_aggs("AAPL", 1, "day", "2022-04-04", "2022-04-04")
+    print(aggs)
+    trades = []
+    for t in client.list_trades("AAA", "2022-04-04", limit=5):
+        trades.append(t)
+    print(trades)
 
 if __name__ == '__main__':
     main()
